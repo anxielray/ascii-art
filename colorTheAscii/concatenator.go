e5ec04c437b41b518119801ef1c8bddd537ee7a6
rogwel <anxielworld@gmail.com>
2024-06-25 04:03:54 +0300

    concatenator update

--- colorTheAscii/concatenator.go
+++ colorTheAscii/concatenator.go
@@ -1,77 +1,89 @@
 package colortheascii
 
-func Concatenator(indx int, A, B, C, D, E, F, G, H []int, color, letters string) (string, string, string, string, string, string, string, string) {
+func Concatenator(Index, A, B, C, D, E, F, G, H []int, color, letters string) (string, string, string, string, string, string, string, string) {
 	var ab, bc, cd, de, ef, fg, gh, hi string
-	for i := range A {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				ab += string(SecondaryMap(A[x], color))
-			}
-		} else {
-			ab += Maps(A[i])
+	for _, index := range Index {
+		var (
+			Aa, Ab, Ac = A[:index], A[index:(len(letters) + index)], A[(len(letters) + index):]
+			Ba, Bb, Bc = B[:index], B[index:(len(letters) + index)], B[(len(letters) + index):]
+			Ca, Cb, Cc = C[:index], C[index:(len(letters) + index)], C[(len(letters) + index):]
+			Da, Db, Dc = D[:index], D[index:(len(letters) + index)], D[(len(letters) + index):]
+			Ea, Eb, Ec = E[:index], E[index:(len(letters) + index)], E[(len(letters) + index):]
+			Fa, Fb, Fc = F[:index], F[index:(len(letters) + index)], F[(len(letters) + index):]
+			Ga, Gb, Gc = G[:index], G[index:(len(letters) + index)], G[(len(letters) + index):]
+			Ha, Hb, Hc = H[:index], H[index:(len(letters) + index)], H[(len(letters) + index):]
+		)
+		for _, a := range Aa {
+			ab += Maps(a)
 		}
-	}
-	for i := range B {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				bc += string(SecondaryMap(B[x], color))
-			}
-		} else {
-			bc += Maps(B[i])
+		for _, a := range Ab {
+			ab += SecondaryMap(a, color)
 		}
-	}
-	for i := range C {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				cd += string(SecondaryMap(C[x], color))
-			}
-		} else {
-			cd += Maps(C[i])
+		for _, a := range Ac {
+			ab += Maps(a)
 		}
-	}
-	for i := range D {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				de += string(SecondaryMap(D[x], color))
-			}
-		} else {
-			de += Maps(D[i])
+		for _, b := range Ba {
+			bc += Maps(b)
 		}
-	}
-	for i := range E {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				ef += string(SecondaryMap(E[x], color))
-			}
-		} else {
-			ef += Maps(E[i])
+		for _, b := range Bb {
+			bc += SecondaryMap(b, color)
 		}
-	}
-	for i := range F {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				fg += string(SecondaryMap(F[x], color))
-			}
-		} else {
-			fg += Maps(F[i])
+		for _, b := range Bc {
+			bc += Maps(b)
 		}
-	}
-	for i := range G {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				gh += string(SecondaryMap(G[x], color))
-			}
-		} else {
-			gh += Maps(G[i])
+		for _, c := range Ca {
+			cd += Maps(c)
 		}
-	}
-	for i := range H {
-		if i == indx {
-			for x := i; x <= (len(letters)+i)-1; x++ {
-				hi += string(SecondaryMap(H[x], color))
-			}
-		} else {
-			hi += Maps(H[i])
+		for _, c := range Cb {
+			cd += SecondaryMap(c, color)
+		}
+		for _, c := range Cc {
+			cd += Maps(c)
+		}
+		for _, d := range Da {
+			de += Maps(d)
+		}
+		for _, d := range Db {
+			de += SecondaryMap(d, color)
+		}
+		for _, d := range Dc {
+			de += Maps(d)
+		}
+		for _, e := range Ea {
+			ef += Maps(e)
+		}
+		for _, e := range Eb {
+			ef += SecondaryMap(e, color)
+		}
+		for _, e := range Ec {
+			ef += Maps(e)
+		}
+		for _, f := range Fa {
+			fg += Maps(f)
+		}
+		for _, f := range Fb {
+			fg += SecondaryMap(f, color)
+		}
+		for _, f := range Fc {
+			fg += Maps(f)
+		}
+		for _, g := range Ga {
+			gh += Maps(g)
+		}
+		for _, g := range Gb {
+			gh += SecondaryMap(g, color)
+		}
+		for _, g := range Gc {
+			gh += Maps(g)
+		}
+		for _, h := range Ha {
+			hi += Maps(h)
+		}
+		for _, h := range Hb {
+			hi += SecondaryMap(h, color)
+		}
+		for _, h := range Hc {
+			hi += Maps(h)
 		}
 	}
 	return " " + ab + "\n", bc + "\n", cd + "\n", de + "\n", ef + "\n", fg + "\n", gh + "\n", hi
