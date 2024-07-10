9c5aeb01ea6e40fd2afe3ca534463a0d3658323e
rogwel <anxielworld@gmail.com>
2024-07-11 02:03:22 +0300

    added other colors and background codes

--- color/colorTheAscii/colors.go
+++ color/colorTheAscii/colors.go
@@ -14,6 +14,26 @@ func GetColor(color string) (code int) {
 		return 35
 	case "cyan":
 		return 36
+	case "black":
+		return 30
+	case "white":
+		return 37
+	case "redbackground":
+		return 41
+	case "greenbackground":
+		return 42
+	case "yellowbackground":
+		return 43
+	case "bluebackground":
+		return 44
+	case "magentabackground":
+		return 45
+	case "cyanbackground":
+		return 46
+	case "blackbackground":
+		return 40
+	case "whitebackground":
+		return 47
 	default:
 		return 0
 	}
