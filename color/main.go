2d7b473bc593a655a6c05ab82ac08c18832c682c
rogwel <anxielworld@gmail.com>
2024-07-11 01:20:55 +0300

    raw main of the code prpoject

---
+++ color/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	co "color/colorTheAscii"
+)
+
+var (
+	colorFlag = flag.String("color", "", "specify the color... e.g, (--color=red)")
+	arguments []string
+	letters   string
+	text      string
+)
+
+func main() {
+	flag.Parse()
+	color := *colorFlag
+	arguments = flag.Args()
+	if len(arguments) == 2 {
+		letters = arguments[0]
+		text = strings.Join(arguments[1:], "")
+		var a, b, c, d, e, f, g, h []int = co.Collector(text)
+		fmt.Println(co.Concatenator(co.IndexTracker(letters, text), a, b, c, d, e, f, g, h, color, letters))
+	} else if len(arguments) == 1 {
+		text = strings.Join(arguments, "")
+		lines := strings.Split(text, "\\n")
+		for _, line := range lines {
+			if line != "" {
+				var a, b, c, d, e, f, g, h []int = co.Collector(text)
+				fmt.Println(co.PlainConcatenator(a, b, c, d, e, f, g, h, color))
+			} else {
+				fmt.Println()
+			}
+		}
+	}
+}
